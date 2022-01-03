1809bda2984b0c7755e35ff4c312ef201c52f8d0
Damjan Dimitrioski <damjandimitrioski@gmail.com>
2022-01-03 16:38:01 +0100

    App routes handling and base layout

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React from "react";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//region Dashboard components
+import NavBar from "./components/dashboard/nav-bar";
+import Sidebar from "./components/dashboard/sidebar";
+import Footer from "./components/dashboard/footer";
+//endregion
+
+//region Auth pages
+import LoginPage from "./components/pages/LoginPage";
+//endregion
+
+//region CRUD pages
+import HomePage from "./components/pages/HomePage";
+import CreateUserPage from "./components/pages/CreateUserPage";
+import DetailUserPage from "./components/pages/DetailUserPage";
+import EditUser from "./components/pages/EditUser";
+//endregion
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <>
+                <header className="page-header" style={{display: 'flex', height: '100vh', overflow: 'scroll initial'}}>
+                    <Sidebar/>
+                </header>
+                <section className="page-content">
+                    <NavBar/>
+                    <section className="mt-6">
+                        {/* Home base root */}
+                        <Route exact path="/">
+                            <HomePage/>
+                        </Route>
+                        {/* List */}
+                        <Route exact path="/users">
+                            <HomePage/>
+                        </Route>
+                        {/* Create */}
+                        <Route exact path="/users/create">
+                            <CreateUserPage/>
+                        </Route>
+                        {/* Read */}
+                        <Route exact path="/users/read/:user">
+                            <DetailUserPage/>
+                        </Route>
+                        {/* Update */}
+                        <Route exact path="/users/edit/:user">
+                            <EditUser/>
+                        </Route>
+                        {/* Login */}
+                        <Route exact path="/login">
+                            <LoginPage/>
+                        </Route>
+                    </section>
+                    <Footer/>
+                </section>
+            </>
+        </Router>
+    );
 }
 
 export default App;
